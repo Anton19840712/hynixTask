609f265625501b44f081464db30d880800ccdbdb
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-20 06:05:10 +0300

    Add configuration helper.

---
+++ HynixTask/HynixTask/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HynixTask.Helpers
+{
+    public static class ConfigurationHelper
+    {
+        public static string GetSettingsElement()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .Build()["Data:Element"];
+        }
+    }
+}
