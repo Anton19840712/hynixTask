ee4eb8ae0819c317ce433ddd98cc79fcbbc893b2
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-20 05:55:18 +0300

    Add data model builder to create data model in a functional way.

---
+++ HynixTask/HynixTask/Models/DataModelBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HynixTask.Models
+{
+    public class DataModelBuilder
+    {
+        public readonly List<Action<DataModel>> Actions = new List<Action<DataModel>>();
+
+        public DataModelBuilder HasFileName(string name)
+        {
+            Actions.Add(dm => { dm.FileName = name; });
+            return this;
+        }
+        public DataModelBuilder HasNumberOfMatches(int number)
+        {
+            Actions.Add(dm => { dm.NumberOfMatches = number; });
+            return this;
+        }
+
+        public DataModel Build()
+        {
+            var dm = new DataModel();
+            Actions.ForEach(a => a(dm));
+            return dm;
+        }
+    }
+}
