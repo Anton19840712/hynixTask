52806c11476fd98b436d4a942c70e315e2f5353c
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-20 06:07:20 +0300

    Add print helper class.

---
+++ HynixTask/HynixTask/Helpers/PrintHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using HynixTask.Framers;
+using HynixTask.Models;
+
+namespace HynixTask.Helpers
+{
+    /// <summary>
+    /// Contains methods to print database info differently.
+    /// </summary>
+    public static class PrinterHelper
+    {
+        public static void PrintMeResults(this IEnumerable<DataModel> elements)
+        {
+            Console.WriteLine("The whole list of files and matches\n");
+
+            var table = new TableFramer("File", "Number of matches");
+
+            foreach (var dataModel in elements)
+            {
+                table.AddRow("#" + dataModel.FileName, dataModel.NumberOfMatches);
+            }
+
+            table.Write();
+        }
+    }
+}
